f61de19598a5db2bc8dca718bc5ffc2e5349d7d4
古田陸太 <rikutaMBA@rikutaMBA.local>
2019-04-07 00:25:48 +0900

    knock82-fix

--- knock82.py
+++ knock82.py
@@ -7,28 +7,32 @@ db = plyvel.DB("./vector1.ldb", create_if_missing=True)
 
 
 def create_db():
-    with open("corpus.txt", "r") as fp:
+    retval={}
+    with open("corpushead.txt", "r") as fp:
         for line in tqdm(fp):
             words = line.lower().strip().split(" ")
             len_words = len(words)
             for i, word in enumerate(words):
-                tmp = db.get(word.encode("utf-8"))
+                tmp = retval.get(word,None)
                 if tmp == None:
                     tmp = {"d": random.randint(1, 5), "c": {}}
-                else:
-                    tmp = json.loads(tmp)
                 d = tmp["d"]
                 c = tmp["c"]
                 cs = words[max(0, i - d):i:] + \
                     words[i + 1:min(i + d, len_words):]
                 for _c in cs:
                     c[_c] = c.get(_c, 0) + 1
-                tmp = json.dumps(tmp)
-                db.put(word.encode('utf-8'), tmp.encode('utf-8'))
+                retval[word]=tmp
+
+def save_db():
+    dict=create_db()
+    for key in dict:
+        db.put(str(key).encode("utf-8"),json.dumps(dict[key]).encode("utf-8"))
+
 
 
 if __name__ == "__main__":
-    create_db()
+    save_db()
     for word, value in db:
         c = json.loads(value)["c"]
         retval = [str(key) for key in c]
